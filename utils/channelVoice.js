58c8d3eab3d5f797364ced9ea6ebc5e253e5fc0b
Coshiloco <pablohurtadohg@gmail.com>
2024-03-21 19:29:18 +0100

    De momento capturo el audio

--- utils/channelVoice.js
+++ utils/channelVoice.js
@@ -1,8 +1,9 @@
-const { joinVoiceChannel, getVoiceConnection, createAudioResource, StreamType, entersState, VoiceConnectionStatus } = require('@discordjs/voice');
+const { createWriteStream } = require('fs');
+const { joinVoiceChannel, entersState, VoiceConnectionStatus, createAudioReceiver } = require('@discordjs/voice');
 
 async function joinChannelAndPrepareForAudioProcessing(interaction) {
     if (!interaction.member.voice.channelId) {
-        await interaction.reply('You need to be in a voice channel.');
+        await interaction.reply({ content: 'You need to be in a voice channel.', ephemeral: true });
         return;
     }
 
@@ -14,17 +15,23 @@ async function joinChannelAndPrepareForAudioProcessing(interaction) {
     });
 
     try {
-        await entersState(connection, VoiceConnectionStatus.Ready, 20e3);
+        await entersState(connection, VoiceConnectionStatus.Ready, 30e3);
+        const receiver = connection.receiver;
 
-        await interaction.followUp('I have joined the voice channel and I am ready to process audio.');
+        // Suscribirse a audio de todos los hablantes. Este es un ejemplo básico.
+        receiver.speaking.on('start', (userId) => {
+            console.log(`User ${userId} started speaking.`);
+            const audioStream = receiver.subscribe(userId);
+            const outputStream = createWriteStream(`./user-audio-${userId}.pcm`);
+            audioStream.pipe(outputStream);
+            audioStream.on('end', () => console.log(`Audio stream for user ${userId} has ended.`));
+        });
 
+        await interaction.followUp({ content: 'I have joined the voice channel and am ready to process audio.' });
     } catch (error) {
         console.error(error);
-        await interaction.followUp('Failed to join the voice channel.');
+        await interaction.followUp({ content: 'Failed to join the voice channel.', ephemeral: true });
     }
 }
 
-
 module.exports.joinChannelAndPrepareForAudioProcessing = joinChannelAndPrepareForAudioProcessing;
-
-
